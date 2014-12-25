38e2948229e6d46d51149dedc2e9f81259818d07
wangtao <wangtao6du@gmail.com>
2014-12-25 19:50:23 +0800

    Initial Commit

---
+++ WTUILocalNotification/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WTUILocalNotification
+//
+//  Created by ZZWangtao on 14-12-25.
+//  Copyright (c) 2014年 ZZWangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WTUILocalNotification/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WTUILocalNotification
+//
+//  Created by ZZWangtao on 14-12-25.
+//  Copyright (c) 2014年 ZZWangtao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
